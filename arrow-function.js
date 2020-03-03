772618068a80cecb62512b55a97039a4eac684fc
saifur703 <saifur703@gmail.com>
2020-03-04 03:04:35 +0600

    Arrow function

---
+++ arrow-function.js
@@ -0,0 +1,24 @@
+const add = (x, y) => x + y;
+console.log(add(12, 56));
+
+const doubleNumb = num => num * num;
+console.log(doubleNumb(5));
+
+const abc = () => 5;
+console.log(abc());
+
+const myFunc = (x, y) => {
+  const add = x + y;
+  const sub = x - y;
+  const result = add * sub;
+  return result;
+};
+console.log(myFunc(7, 5));
+
+let str1 = 'Hello ';
+let str2 = 'World';
+let str = str1.concat(str2);
+console.log(str);
+
+let $test = 'Test';
+console.log($test);
