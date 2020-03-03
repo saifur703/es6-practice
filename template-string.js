3ad2b51b83c34f045baaa101703caee3cd789dfe
saifur703 <saifur703@gmail.com>
2020-03-04 03:05:56 +0600

    ES6 Template String

---
+++ template-string.js
@@ -0,0 +1,11 @@
+const fname = 'Saifur';
+const lname = 'Rahman';
+const fullName = fname + ' ' + lname;
+console.log(fullName);
+
+console.log(`My First Name is ${fname} and Last Name is ${lname}`);
+
+const name = 'Adam Sandler';
+const greeting = `Hello ${name}
+, welcome to the club`;
+console.log(greeting);
